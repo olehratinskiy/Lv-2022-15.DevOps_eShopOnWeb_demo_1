b19b4d772600426eb5a12b985d0517dfab605fd2
Oleh Ratinskiy <91226347+olehratinskiy@users.noreply.github.com>
2022-07-29 01:45:00 +0300

    create logins_creation.sql

---
+++ logins_creation.sql
@@ -0,0 +1,24 @@
+USE [master];
+GO
+CREATE LOGIN WebUser 
+    WITH PASSWORD    = N'w982757892&',
+    CHECK_POLICY     = ON,
+    CHECK_EXPIRATION = OFF;
+GO
+EXEC sp_addsrvrolemember 
+    @loginame = N'WebUser', 
+    @rolename = N'sysadmin';
+GO
+CREATE LOGIN PublicApiUser
+    WITH PASSWORD    = N'pa13456286*',
+    CHECK_POLICY     = ON,
+    CHECK_EXPIRATION = OFF;
+GO
+EXEC sp_addsrvrolemember 
+    @loginame = N'PublicApiUser', 
+    @rolename = N'sysadmin';
+GO
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', 
+     N'Software\Microsoft\MSSQLServer\MSSQLServer',
+     N'LoginMode', REG_DWORD, 2;
+exit
